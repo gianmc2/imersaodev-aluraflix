52fd2bd43447628cb952e4dda545417e01593ec0
Gian <80715415+gianmc2@users.noreply.github.com>
2021-03-26 16:17:37 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,16 @@
-var nomesFilmes = ["Rush", "Interstellar", "Rogue One", "Uncut Gems"]
-var listaFilmes = ["https://m.media-amazon.com/images/M/MV5BOWEwODJmZDItYTNmZC00OGM4LThlNDktOTQzZjIzMGQxODA4XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BMjEwMzMxODIzOV5BMl5BanBnXkFtZTgwNzg3OTAzMDI@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BZDhkMjUyYjItYWVkYi00YTM5LWE4MGEtY2FlMjA3OThlYmZhXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_UX182_CR0,0,182,268_AL_.jpg"]
+function adicionarFilme() {
+  var campoFilmeFavorito = document.querySelector('#filme')
+  var filmeFavorito = campoFilmeFavorito.value
+  if (filmeFavorito.endsWith('.jpg')){
+    listarFilmesNaTela(filmeFavorito)
+  } else {
+    alert('Imagem inválida.')
+  }
+  campoFilmeFavorito.value = ""
+}
 
-for (var i = 0; i < listaFilmes.length; i++){
-  document.write("<div class = 'box' > <img src=" + listaFilmes[i] + "><p>" + nomesFilmes[i] + "</p></div>")
+function listarFilmesNaTela(filme) {
+  var listaFilmes = document.querySelector('#listaFilmes')
+  var elementoFilme = "<img src=" + filme + ">"
+  listaFilmes.innerHTML = listaFilmes.innerHTML + elementoFilme
 }
