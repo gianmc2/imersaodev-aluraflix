f1b7e6e7affad7d98afa5078d0d3353328f0fa24
Gian <80715415+gianmc2@users.noreply.github.com>
2021-03-26 15:50:17 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,6 @@
+var nomesFilmes = ["Rush", "Interstellar", "Rogue One", "Uncut Gems"]
+var listaFilmes = ["https://m.media-amazon.com/images/M/MV5BOWEwODJmZDItYTNmZC00OGM4LThlNDktOTQzZjIzMGQxODA4XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BMjEwMzMxODIzOV5BMl5BanBnXkFtZTgwNzg3OTAzMDI@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BZDhkMjUyYjItYWVkYi00YTM5LWE4MGEtY2FlMjA3OThlYmZhXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_UX182_CR0,0,182,268_AL_.jpg"]
+
+for (var i = 0; i < listaFilmes.length; i++){
+  document.write("<div class = 'box' > <img src=" + listaFilmes[i] + "><p>" + nomesFilmes[i] + "</p></div>")
+}
